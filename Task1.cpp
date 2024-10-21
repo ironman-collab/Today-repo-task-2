7b8e3dbc7feea75f775fa4a580e04ad727f8a07b
ironman-collab <uk6118660@gmail.com>
2024-10-21 16:42:20 +0500

    umar.c++

---
+++ Task1.cpp
@@ -0,0 +1,138 @@
+//Umar Khalid
+//SEction A_2
+//Task-1
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout << "************" << endl;
+	cout << "Hello World" << endl;
+	cout << "************" << endl;
+	return 0;
+}
+ Task-2
+{
+	cout << "************" << endl;
+	cout << "*          *" << endl;
+	cout << "************" << endl;
+	return 0;
+}
+Task-3
+{
+
+	cout << "    *" << endl;
+	cout << "   * *" << endl;
+	cout << "  *   *" << endl;
+	cout << " *******" << endl;
+	return 0;
+}
+//Task-4
+
+{
+
+	cout << "   *" << endl;
+	cout << "  ***" << endl;
+	cout << " *****" << endl;
+	cout << "*******" << endl;
+	return 0;
+}
+//Task-5
+	{
+		 cout << " ****" << endl;
+		cout << "*    *" << endl;
+		cout << "*    *" << endl;
+		cout << " **** " << endl;
+		return 0;
+	}
+	/*TAsk-6*/
+{
+	cout << " *******" << endl;
+	cout << " *     *" << endl;
+	cout << " *     *" << endl;
+	cout << " ******* " << endl;
+	return 0;
+}
+
+//Task-7
+{
+	cout << "   *" << endl;
+	cout << " *   *" << endl;
+	cout << "*     *" << endl;
+	cout << " *   *" << endl;
+	cout << "   *" << endl;
+	return 0;
+}
+//Task-8
+{
+	cout << "    *" << endl;
+	cout << "  *****" << endl;
+	cout << "*********" << endl;
+	cout << "  *****" << endl;
+	cout << "    *" << endl;
+	return 0;
+}
+//Task-9
+
+	{
+		cout << "@@@@@@@@@@@@@@" << endl;
+		cout << "@  @      @  @" << endl;
+		cout << "@ @        @ @" << endl;
+		cout << "@@          @@" << endl;
+		cout << "@            @" << endl;
+		cout << "@@@@@@@@@@@@@@" << endl;
+		return 0;
+	}
+//Task-10
+{
+	cout << "****************" << endl;
+	cout << "         *" << endl;
+	cout << "         *" << endl;
+	cout << "         *" << endl;
+	cout << "         *" << endl;
+	cout << "         *" << endl;
+	cout << "         *" << endl;
+	cout << "****************" << endl;
+	\n
+	cout << "****************" << endl;
+	cout << "         *" << endl;
+	cout << "         *" << endl;
+	cout << "         *" << endl;
+	cout << "         *" << endl;
+	cout << "         *" << endl;
+	cout << "         *" << endl;
+	cout << "         *" << endl;
+	cout << "         *" << endl;
+	cout << "****************" << endl;
+	\n
+	cout << "****************" << endl;
+	cout << "*" << endl;
+	cout << "*" << endl;
+	cout << "*" << endl;
+	cout << "*" << endl;
+	cout << "*" << endl;
+	cout << "*" << endl;
+	cout << "*" << endl;
+	cout << "*" << endl;
+	return 0;
+	}
+//Task-11
+{
+	cout << "Name:Umar khalid" << endl;
+	cout << "University ID:L1F24BSCS0154" << endl;
+	cout << "Muhammad khalid" << endl;
+	cout << "Phone No:03164618928" << endl;
+	cout << "Block H,House #3,street #3,Johar Town,Lahore" << endl;
+	cout << "Samester:1" << endl;
+	cout << "Introduction to computing " << endl;
+	return 0;
+}
+//Task-12
+{
+		cout << "***********************************" << endl;
+		cout << "*     Programmimg assighment1     *" << endl;
+		cout << "*     computer Programming        *" << endl;
+		cout << "*         Author:???              *" << endl;
+		cout << "*    Due Date: Thusrsday,4        *" << endl;
+		cout << "***********************************" << endl;
+		return 0;
+}
